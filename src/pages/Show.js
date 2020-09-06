d16d53e5bd106aed5f9d9d35a51f5e44c6a4df47
Mark Mariscal <markamariscal@gmail.com>
2020-09-06 10:29:56 -0700

    add loading and error message to show

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,13 +7,47 @@ const Show = () => {
 
   const [show, setShow] = useState(null);
 
+  // boolean value if data is being loaded or not
+  const [isLoading, setIsLoading] = useState(true);
+
+  // for errors
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    // checks if our page is rendered on the page or not becasuse useEffect callback only renders when componoent is mounted
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        // if component is mounted only then we will set state
+        if (isMounted) {
+          setShow(results);
+          // after setting state for show set state for Loading
+          // we will set the stae for data being loaded and set it to false
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
 
   return <div>This is show page</div>;
 };
